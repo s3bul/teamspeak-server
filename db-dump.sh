cae94e73253af8ea948cf4047425ff397441fd99
Sebastian Korzeniecki <seba5zer@gmail.com>
2021-02-02 20:32:40 +0100

    Add backup dir

--- db-dump.sh
+++ db-dump.sh
@@ -1,3 +1,3 @@
 docker exec "$(docker ps -qf "name=teamspeak_db")" \
   mysqldump -p"$(grep MYSQL_ROOT_PASSWORD .env.db | sed -E 's/(.*=)(.*)/\2/')" \
-  teamspeak >"teamspeak_db-$(date +%Y%m%d%H%M%S).sql"
+  teamspeak >"backup/teamspeak_db-$(date +%Y%m%d%H%M%S).sql"
