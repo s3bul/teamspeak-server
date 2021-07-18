702713cfcf00fb3598a3c31a8ce42ac8aafd863d
Sebastian Korzeniecki <seba5zer@gmail.com>
2021-07-18 18:56:47 +0200

    Fix dump script

--- db-dump.sh
+++ db-dump.sh
@@ -2,7 +2,7 @@
 
 backupFile="teamspeak_db-$(date +%Y%m%d%H%M%S).sql"
 docker exec "$(docker ps -qf "name=teamspeak_db")" \
-  mysqldump -p"$(grep MYSQL_ROOT_PASSWORD .env.db | sed -E 's/(.*=)(.*)/\2/')" \
+  mysqldump -p"$(cat secrets/db_root_password.txt)" \
   teamspeak >"${backupFile}" && \
   tar -czf "${backupFile}.tar.gz" "${backupFile}" && \
   mv "${backupFile}.tar.gz" backup && \
