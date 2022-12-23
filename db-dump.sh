9c5f617c26f315a3cb6a13466c8f98402566bbae
Sebastian Korzeniecki <seba5zer@gmail.com>
2022-12-23 13:23:19 +0100

    Refactoring - move docker dirs

--- db-dump.sh
+++ db-dump.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-backupFile="teamspeak_db-$(date +%Y%m%d%H%M%S).sql"
+backupFile="teamspeak_db-$(date +%Y%m%d%H%M%S).sql.gz"
 docker exec "$(docker ps -qf "name=teamspeak_db")" \
   mysqldump -p"$(cat secrets/db_root_password.txt)" \
-  teamspeak >"${backupFile}" && \
-  tar -czf "${backupFile}.tar.gz" "${backupFile}" && \
-  mv "${backupFile}.tar.gz" backup && \
-  rm -rf "${backupFile}"
+  teamspeak | gzip >"${backupFile}" && \
+  mv "${backupFile}" backup
